9422feb16b0941281f9b9d4e011478b66ebc4a55
tritranduc <tritranduc11@gmail.com>
2022-04-07 13:43:38 +0700

    config somethings

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,23 +1,29 @@
-import Navbar from "./../components/Navbar";
-import { GetPostsDocument } from "../generated/graphql";
-import { addApolloState, initializeApollo } from "../lib/apolloClient";
-import { useGetPostsQuery } from './../generated/graphql';
+import Navbar from './../components/Navbar'
+import { GetPostsDocument } from '../generated/graphql'
+import { addApolloState, initializeApollo } from '../lib/apolloClient'
+import { useGetPostsQuery } from './../generated/graphql'
 
 const Index = () => {
-  const {data,loading} = useGetPostsQuery()
+  const { data, loading } = useGetPostsQuery()
 
-  return <div>
-    <Navbar />
-    {
-      loading ? "LOADING...":(
-        <>{data?.getPosts?.posts?.map(post => <div>{post.title}</div>)}</>
-      )
-    }
-  </div>
+  return (
+    <div>
+      <Navbar />
+      {loading ? (
+        'LOADING...'
+      ) : (
+        <>
+          {data?.getPosts?.posts?.map((post) => (
+            <div key={post._id}>{post.title}</div>
+          ))}
+        </>
+      )}
+    </div>
+  )
 }
 
-export default Index;
-export const getStaticProps = async () => { 
+export default Index
+export const getStaticProps = async () => {
   const apolloClient = initializeApollo()
 
   await apolloClient.query({
